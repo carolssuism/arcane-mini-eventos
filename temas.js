175382abfc32bc97821fc0233562ce2bfba3f45e
carolssuism <136553010+carolssuism@users.noreply.github.com>
2026-09-11 18:52:38 -0300

    Publicar mini-eventos v0.1.15

--- temas.js
+++ temas.js
@@ -4,7 +4,7 @@
 (function (window) {
   'use strict';
   window.ArcaneMiniEventosTemas = {
-    versao: '0.1.13',
+    versao: '0.1.15',
     eventos: {
     'blecaute': {
       nome: 'Blecaute em Hogwarts',
