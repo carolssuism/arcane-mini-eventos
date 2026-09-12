261ce9f4467c37fd3e290f8e01c54fbb06230f06
carolssuism <136553010+carolssuism@users.noreply.github.com>
2026-09-12 02:33:26 -0300

    Add files via upload

--- loader.js
+++ loader.js
@@ -6,7 +6,7 @@
   window.__ARCANE_MINI_EVENTOS_LOADER__ = true;
 
   var REPOSITORIO = 'carolssuism/arcane-mini-eventos';
-  var VERSAO_RESERVA = 'v0.1.29';
+  var VERSAO_RESERVA = 'v0.1.30';
   var TIMEOUT = 12000;
   var base = '';
 
